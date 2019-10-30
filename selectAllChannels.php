35470486b58317f542668506ea4710637431db8c
Manuel <msalaguila@gmail.com>
2019-10-30 16:22:21 +0000

    Fixing bugs

--- selectAllChannels.php
+++ selectAllChannels.php
@@ -1,6 +1,7 @@
 <?php
 
-function getInformationFromDatabase() {
+function getInformationFromDatabase()
+{
 
     $host = "localhost";
     $database = "laboratorioweb";
@@ -9,7 +10,7 @@ function getInformationFromDatabase() {
 
     $fechaToStore = date("Y-m-d H:i:s");
 
-        // 1 Stablishing connection to Database
+    // 1 Stablishing connection to Database
     $connection = mysqli_connect($host, $user, $databasePassword, $database);
 
     // 2. Managing errors
@@ -17,26 +18,37 @@ function getInformationFromDatabase() {
     if (mysqli_connect_errno()) {
         die(mysqli_connect_error());
     }
-    
+
     // 4. Checking if the table is created
 
-    $sqlUser = "SELECT * FROM canales"; 
-     
+    $sqlCanales = "SELECT * FROM canales";
+
     $nombreCanal = "";
     $descripcionCanal = "";
     $fechaCanal = "";
     $urlCanal = "";
+    $idUser = 0;
 
-    if ($result = mysqli_query($connection, $sqlUser)) {
+    if ($result = mysqli_query($connection, $sqlCanales)) {
         while ($row = mysqli_fetch_array($result)) {
             $nombreCanal = $row["nombreCanal"];
             $descripcionCanal = $row["descripcion"];
             $fechaCanal = $row["fechaRegistro"];
             $urlCanal = $row["url"];
+            $idUser = $row["id_user"];
+
+
+            $sqlUser = "SELECT * FROM users WHERE id='$idUser'";
+            $userName = 0;
+            if ($result2 = mysqli_query($connection, $sqlUser)) {
+                if ($row2 = mysqli_fetch_assoc($result2)) {
+                    $userName = $row2["nombre"];
+                }
+            }
 
             echo "<article class='canales_article'>";
             echo "<p id='paragraph_canales'>Información sobre el Canal: $nombreCanal</p>";
-            echo "<p id='paragraph_canales'>Autor: &nbsp;</p>";
+            echo "<p id='paragraph_canales'>Autor: $userName&nbsp;</p>";
             echo "<p id='paragraph_canales'>Descripción: $descripcionCanal</p>";
             echo "<p id='paragraph_canales'>Fecha: $fechaCanal</p>";
             echo "<p id='paragraph_canales'>Enlace URL: $urlCanal</p>";
@@ -45,7 +57,6 @@ function getInformationFromDatabase() {
     }
 
     mysqli_close($connection);
-    }
+}
 
-    getInformationFromDatabase();
-?>
+getInformationFromDatabase();
--- selectAllChannelsUserLogin.php
+++ selectAllChannelsUserLogin.php
@@ -1,46 +1,55 @@
 <?php
-                $host = "localhost";
-                $database = "laboratorioweb";
-                $user = "root";
-                $databasePassword = "";
-            
-                $fechaToStore = date("Y-m-d H:i:s");
-            
-                    // 1 Stablishing connection to Database
-                $connection = mysqli_connect($host, $user, $databasePassword, $database);
-            
-                // 2. Managing errors
-            
-                if (mysqli_connect_errno()) {
-                    die(mysqli_connect_error());
-                }
-                
-                // 4. Checking if the table is created
-            
-                $sqlUser = "SELECT * FROM canales"; 
-                 
-                $nombreCanal = "";
-                $descripcionCanal = "";
-                $fechaCanal = "";
-                $urlCanal = "";
-            
-                if ($result = mysqli_query($connection, $sqlUser)) {
-                    while ($row = mysqli_fetch_array($result)) {
-                        $nombreCanal = $row["nombreCanal"];
-                        $descripcionCanal = $row["descripcion"];
-                        $fechaCanal = $row["fechaRegistro"];
-                        $urlCanal = $row["url"];
+$host = "localhost";
+$database = "laboratorioweb";
+$user = "root";
+$databasePassword = "";
 
-                        echo "<button class='btn btn-danger float-right deleteButton' type='button'></button>";
-                        echo "<article class='canales_article'>";
-                        echo "<p id='paragraph_canales'>Información sobre el Canal: $nombreCanal</p>";
-                        echo "<p id='paragraph_canales'>Autor: &nbsp;</p>";
-                        echo "<p id='paragraph_canales'>Descripción: $descripcionCanal</p>";
-                        echo "<p id='paragraph_canales'>Fecha: $fechaCanal</p>";
-                        echo "<p id='paragraph_canales'>Enlace URL: $urlCanal</p>";
-                        echo "</article>";
-                    }
-                }
-            
-                mysqli_close($connection);
-            ?>
+$fechaToStore = date("Y-m-d H:i:s");
+
+// 1 Stablishing connection to Database
+$connection = mysqli_connect($host, $user, $databasePassword, $database);
+
+// 2. Managing errors
+
+if (mysqli_connect_errno()) {
+    die(mysqli_connect_error());
+}
+
+// 4. Checking if the table is created
+
+$sqlUser = "SELECT * FROM canales";
+
+$nombreCanal = "";
+$descripcionCanal = "";
+$fechaCanal = "";
+$urlCanal = "";
+$idUser = 0;
+
+if ($result = mysqli_query($connection, $sqlUser)) {
+    while ($row = mysqli_fetch_array($result)) {
+        $nombreCanal = $row["nombreCanal"];
+        $descripcionCanal = $row["descripcion"];
+        $fechaCanal = $row["fechaRegistro"];
+        $urlCanal = $row["url"];
+        $idUser = $row["id_user"];
+
+        $sqlUser = "SELECT * FROM users WHERE id='$idUser'";
+        $userName = 0;
+        if ($result2 = mysqli_query($connection, $sqlUser)) {
+            if ($row2 = mysqli_fetch_assoc($result2)) {
+                $userName = $row2["nombre"];
+            }
+        }
+
+        echo "<button class='btn btn-danger float-right deleteButton' type='button'></button>";
+        echo "<article class='canales_article'>";
+        echo "<p id='paragraph_canales'>Información sobre el Canal: $nombreCanal</p>";
+        echo "<p id='paragraph_canales'>Autor: $userName&nbsp;</p>";
+        echo "<p id='paragraph_canales'>Descripción: $descripcionCanal</p>";
+        echo "<p id='paragraph_canales'>Fecha: $fechaCanal</p>";
+        echo "<p id='paragraph_canales'>Enlace URL: $urlCanal</p>";
+        echo "</article>";
+    }
+}
+
+mysqli_close($connection);
