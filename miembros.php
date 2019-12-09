6b2a84d342a1fe5a9c9d5b76e2837ea963a02207
Manuel <msalaguila@gmail.com>
2019-12-09 13:57:36 +0000

    Creando relaciones entre miembros

--- miembros.php
+++ miembros.php
@@ -63,11 +63,7 @@
                 <div class="col article1_shop">
                     <article class="text-break article1_shop">
                         <h1 class="article1_shop_heading">Miembros registrados en la red social</h1>
-                        <p class="parrafo-miembros-social">Miembro 1</p>
-                        <p class="parrafo-miembros-social">Miembro 1</p>
-                        <p class="parrafo-miembros-social">Miembro 1</p>
-                        <p class="parrafo-miembros-social">Miembro 1</p>
-                        <p class="parrafo-miembros-social">Miembro 1</p>
+                        <?php include('./scripts/showAllMembers.php');?>
                     </article>
                 </div>
             </div>
